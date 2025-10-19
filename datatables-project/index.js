62d81384efff7ab0b2f5bfaa6bd159b5eba1d2ba
raman-sagar <ramansagar938923@gmail.com>
2025-10-19 14:02:30 +0530

    initial commit

--- datatables-project/index.js
+++ datatables-project/index.js
@@ -1,21 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const User = require('./models/users.model');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const User = require("./models/users.model");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded());
+app.use(express.static("public"));
 
 // Connect MongoDB
-mongoose.connect('mongodb://localhost:27017/users_demo')
-.then(() => console.log('Database Connected!'));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/users_demo")
+  .then(() => console.log("Database Connected!"));
 
 // Get all users
-app.get('/api/users', async (req, res) => {
+
+app.get("/api/users", async (req, res) => {
   const users = await User.find();
   res.json({ data: users });
 });
 
-app.listen(3000, () => console.log('Server started'));
+//Add record in users_demo database
+app.post("/api/users", async (req, res) => {
+  try {
+    const record = await User.create(req.body);
+    if (!record) {
+      return res.status(404).json({ message: "Record not inserted" });
+    }
+    res.json({ record });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server started"));
--- datatables-project/models/users.model.js
+++ datatables-project/models/users.model.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
-  age: Number
+  age: Number,
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
